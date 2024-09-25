7979dcf8f02518d8a4adb9fb566dd47ef2d73d02
李子铭 <zi__mm@163.com>
2024-09-25 09:44:52 +0800

    sdk封装

--- api/v1/key/acquire.go
+++ api/v1/key/acquire.go
@@ -8,7 +8,7 @@ type Acquire api.Service
 
 const method = "/openapi/v1/key/acquire"
 
-func (a *Acquire) Handle(request *AcquireRequest) (*AcquireReply, error) {
+func (a *Acquire) Handle(request *AcquireRequest) (*Reply, error) {
 	a.Request(method, nil)
 	return nil, nil
 }
