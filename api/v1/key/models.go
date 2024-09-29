d44d62e868cecff4398cb4d9913d8105c0119e27
李子铭 <zi__mm@163.com>
2024-09-29 09:42:33 +0800

    sdk优化

--- api/v1/key/models.go
+++ api/v1/key/models.go
@@ -35,8 +35,8 @@ func (c *OrderRequest) Validate() error {
 }
 
 type QueryRequest struct {
-	OutBizNo string `json:"out_biz_no"` // out_biz_no/trade_no二选一
-	TradeNo  string `json:"trade_no"`   // // out_biz_no/trade_no二选一 若不为空，则优先使用
+	OutBizNo string `json:"out_biz_no,omitempty"` // out_biz_no/trade_no二选一
+	TradeNo  string `json:"trade_no,omitempty"`   // out_biz_no/trade_no二选一 若不为空，则优先使用
 }
 
 func (a *QueryRequest) String() (string, error) {
@@ -47,19 +47,17 @@ func (a *QueryRequest) String() (string, error) {
 	return string(b), nil
 }
 
-func (c *QueryRequest) Validate() error {
-	if err := validator.New().Struct(c); err != nil {
-		for _, err = range err.(validator.ValidationErrors) {
-			return fmt.Errorf(err.Error())
-		}
+func (q *QueryRequest) Validate() error {
+	if q.OutBizNo == "" && q.TradeNo == "" {
+		return fmt.Errorf("out_biz_no/trade_no 二选一")
 	}
 	return nil
 }
 
 type DiscardRequest struct {
-	OutRequestNo string `validate:"required,min=2,max=30" json:"out_request_no"`
-	OutBizNo     string `json:"out_biz_no,omitempty"` // out_biz_no/trade_no二选一
-	TradeNo      string `json:"trade_no,omitempty"`   // out_biz_no/trade_no二选一 若不为空，则优先使用
+	OutBizNo string `json:"out_biz_no,omitempty"` // out_biz_no/trade_no二选一
+	TradeNo  string `json:"trade_no,omitempty"`   // out_biz_no/trade_no二选一 若不为空，则优先使用
+	Reason   string `json:"reason,omitempty"`     // 可为空
 }
 
 func (a *DiscardRequest) String() (string, error) {
@@ -71,11 +69,6 @@ func (a *DiscardRequest) String() (string, error) {
 }
 
 func (c *DiscardRequest) Validate() error {
-	if err := validator.New().Struct(c); err != nil {
-		for _, err = range err.(validator.ValidationErrors) {
-			return fmt.Errorf(err.Error())
-		}
-	}
 	if c.OutBizNo == "" && c.TradeNo == "" {
 		return fmt.Errorf("out_biz_no/trade_no 二选一")
 	}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sleepinggodoflove/lansexiongdi-marketing-sdk
 go 1.22.2
 
 require (
+	github.com/go-playground/validator/v10 v10.22.1
 	github.com/stretchr/testify v1.9.0
 	github.com/tjfoc/gmsm v1.4.1
 )
@@ -12,7 +13,6 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.22.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
