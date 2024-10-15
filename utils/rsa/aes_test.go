9ca6e0af55ff3ae244062bb0ed26ce377440a33a
李子铭 <zi__mm@163.com>
2024-10-15 14:44:14 +0800

    sdk优化 .

--- utils/rsa/aes_test.go
+++ utils/rsa/aes_test.go
@@ -8,12 +8,20 @@ func TestGenerateAesKey(t *testing.T) {
 }
 
 func TestEncode(t *testing.T) {
+	key := "870abfc720f86ce2c5e4d3345741d48d"
 	str := "123yie一二三"
-	e := Encode("870abfc720f86ce2c5e4d3345741d48d", str)
+	e := Encode(key, str)
 	t.Log(e)
 }
 
 func TestDecode(t *testing.T) {
+	key := "870abfc720f86ce2c5e4d3345741d48d"
+	e := "xxxx"
+	d := Decode(key, e)
+	t.Log(d)
+}
+
+func TestEncodeDecode(t *testing.T) {
 	key := "870abfc720f86ce2c5e4d3345741d48d"
 	str := "123yie一二三"
 	e := Encode(key, str)
